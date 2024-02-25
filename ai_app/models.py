2854fcbd1dce9d2677e2c9355d36054577199b54
Damian Grembocki <damian.grembocki@o2.pl>
2024-02-25 21:21:46 +0100

    django base and imageModel

---
+++ ai_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ImageElement(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=False, default='')
+    image = models.ImageField(upload_to='media', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
